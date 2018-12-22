4832afe31700565c862dfe9c77938b8ee1eb1081
YanbinDeng <dyb0214@gmail.com>
2018-12-21 22:47:49 -0800

    individual form input validation onChange implemented

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,40 +1,61 @@
 import React, { Component } from "react";
-import Input from "./common/Input"
+import Input from "./common/Input";
+
 class LoginForm extends Component {
-// error is an object that holds all the 
+  // error is an object that holds all the
   state = {
     account: { username: "", password: "" },
-    errors : {
-
-    }
+    errors: {}
   };
 
   validate = () => {
-      const errors = {};
-      if (this.state.account.username.trim() === '') {
-          errors.username = "username is required"
-      }
-      if (this.state.account.password.trim() === '') {
-        errors.password = "password is required"
+    const errors = {};
+    if (this.state.account.username.trim() === "") {
+      errors.username = "username is required";
     }
-      // Object.keys(errors) will return all the key of the error object 
-      return Object.keys(errors).length === 0 ? null : errors;
-  }
+    if (this.state.account.password.trim() === "") {
+      errors.password = "password is required";
+    }
+    // Object.keys(errors) will return all the keys of the error object
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
   handleSubmit = e => {
     e.preventDefault();
     const errors = this.validate();
-    // console.log(errors)
-    this.setState({errors : errors || {}}); 
-    // can not just set state to errors because it being pass into the <Input /> as a props, undifine prop cause error 
+    this.setState({ errors: errors || {} }); // of erros is not an empty object, return, else return an empty object for error
+    // can not just set state to errors because its being pass into the <Input /> as a props, an undefined prop will cause error
     if (errors) return;
-    
   };
 
-  handleChange = ({currentTarget : input}) => {
+  /*
+    validProperty will valid individual input, if input change from valid to invalid, it will update the error state
+  */
+  validProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") {
+        return "Username is required.";
+      }
+    }
+    if (name === "password") {
+      if (value.trim() === "") {
+        return "Password is requird.";
+      }
+    }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validProperty(input);
+
+    if (errorMessage) {
+      console.log(errorMessage);
+      errors[input.name] = errorMessage;
+    } else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
