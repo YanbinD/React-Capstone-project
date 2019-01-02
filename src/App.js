0bffaf3b7ec767caa466ff9dd8d977622ca1adba
Yanbin Deng <dyb0214@gmail.com>
2019-01-02 08:27:42 -0800

    added httpservices

--- src/App.js
+++ src/App.js
@@ -1,73 +1,103 @@
 import React, { Component } from "react";
-import {Route, Switch, Redirect, Link} from 'react-router-dom';
-import NavBar from "./Components/navBar"
+import { Route, Switch, Redirect, Link } from "react-router-dom";
+import NavBar from "./Components/navBar";
+import { ToastContainer } from "react-toastify";
 //swotch will render the first path that matches
 import MoviesPanel from "./Components/MoviesPanel";
-import CountersPanel from "./Components/Counters_group/CountersPanel"
-import LoginForm from "./Components/LoginForm_joy"
-import RegisterForm from "./Components/RegisterForm"
-import MovieForm from "./Components/MovieForm"
+import CountersPanel from "./Components/Counters_group/CountersPanel";
+import LoginForm from "./Components/LoginForm_joy";
+import RegisterForm from "./Components/RegisterForm";
+import MovieForm from "./Components/MovieForm";
 import "./App.css";
-
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
-
   render() {
     return (
       <React.Fragment>
-          <NavBar/> 
-          <main className="container">
-
-              <Switch>
-                <Route path = "/Home" component={Home} />  
-                <Route path="/movies/:id" component={MovieForm} />
-                <Route path = "/Movies" component={MoviesPanel}/>
-                <Redirect from="/movie" to="/Movies" />
-                <Route path = "/login" component={LoginForm} />
-                <Route path = "/register" component={RegisterForm} />
-                <Route path = "/Counters" component={CountersPanel} />
-                <Redirect from="/counter" to="/Counters" />
-                <Route path = "/both" render={ () => 
-                  <div>
-                    <CountersPanel />
-                    <MoviesPanel />
-                </div>}/>
-                <Route path ="/not-found" exact component={NotFound} />
-                <Redirect to="/Home" />
-              </Switch>
-          </main>
+        <ToastContainer />
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/Home" component={Home} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/Movies" component={MoviesPanel} />
+            <Redirect from="/movie" to="/Movies" />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/Counters" component={CountersPanel} />
+            <Redirect from="/counter" to="/Counters" />
+            <Route
+              path="/both"
+              render={() => (
+                <div>
+                  <CountersPanel />
+                  <MoviesPanel />
+                </div>
+              )}
+            />
+            <Route path="/not-found" exact component={NotFound} />
+            <Redirect to="/Home" />
+          </Switch>
+        </main>
       </React.Fragment>
-
     );
   }
 }
 
 const Home = () => {
-  return ( 
+  return (
     <div className="jumbotronMy">
       <h1 className="display-4">Welcome!</h1>
       <p className="lead">This is a simple react demo program</p>
-      <hr className="my-3"/>
+      <hr className="my-3" />
       <p> Choose one of the routes below </p>
-      <Link to="/both"> <button type="button" className="btn btn-outline-primary" >/Both</button> </Link>
-      <Link to="/Movies"> <button type="button" className="btn btn-outline-primary" >/Movies</button> </Link>
-      <Link to="/Counters"> <button type="button" className="btn btn-outline-primary" >/Counters</button></Link>
-      <Link to="/movie"> <button type="button" className="btn btn-outline-primary" >/movie (alias) </button> </Link>
-      <Link to="/counter"> <button type="button" className="btn btn-outline-primary" >/counter (alias) </button> </Link>
-      <Link to="/asdasdf"> <button type="button" className="btn btn-outline-primary" >/random undefined route </button>  </Link>
-      <a className="btn btn-primary btn-sm" href="/both" role="button">Anchor Tag</a>
+      <Link to="/both">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /Both
+        </button>{" "}
+      </Link>
+      <Link to="/Movies">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /Movies
+        </button>{" "}
+      </Link>
+      <Link to="/Counters">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /Counters
+        </button>
+      </Link>
+      <Link to="/movie">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /movie (alias){" "}
+        </button>{" "}
+      </Link>
+      <Link to="/counter">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /counter (alias){" "}
+        </button>{" "}
+      </Link>
+      <Link to="/asdasdf">
+        {" "}
+        <button type="button" className="btn btn-outline-primary">
+          /random undefined route{" "}
+        </button>{" "}
+      </Link>
+      <a className="btn btn-primary btn-sm" href="/both" role="button">
+        Anchor Tag
+      </a>
     </div>
-   );
-}
-
+  );
+};
 
 const NotFound = () => {
-  return (
-    <h3> undefined route </h3>
-  )
-}
- 
+  return <h3> undefined route </h3>;
+};
 
 // <Movies />
 export default App;
-
--- src/Components/common/form.jsx
+++ src/Components/common/form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
-import Input from "./Input";
+import Input from "./input";
 import Select from "./select";
 class Form extends Component {
   state = {
